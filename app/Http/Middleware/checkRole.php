438b2b5a78e5bc773a655d2a78141d48ca59eec6
DeaNabilaHusna <deanabilahusna@gmail.com>
2024-05-26 19:24:16 +0700

    fix: Resolve bug in collaborator role management and improve project page layout

--- app/Http/Middleware/checkRole.php
+++ app/Http/Middleware/checkRole.php
@@ -6,6 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
+use Spatie\Permission\Traits\HasRoles;
 
 class checkRole
 {
@@ -14,21 +15,48 @@ class checkRole
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next, $roles): Response
+    // public function handle(Request $request, Closure $next, $roles): Response
+    // {
+    //     $roles = array('pic', 'analyst', 'designer', 'programmer', 'mentor', 'client');
+    //     if (!auth()->check()) {
+    //         return redirect()->route('login');
+    //     }
+        
+    //     $user = auth()->user();
+        
+    //     foreach ($roles as $role) {
+    //         if ($user->role === $role) {
+    //             return $next($request);
+    //         }
+    //     }
+        
+    //     return redirect()->route('unauthorized');
+    // }
+    public function handle(Request $request, Closure $next, $role): Response
     {
-        $roles = array('pic', 'analyst', 'designer', 'programmer', 'mentor', 'client');
         if (!auth()->check()) {
             return redirect()->route('login');
         }
         
         $user = auth()->user();
-        
-        foreach ($roles as $role) {
-            if ($user->role === $role) {
-                return $next($request);
-            }
+
+        if ($user->role === $role) {
+            return $next($request);
         }
         
         return redirect()->route('unauthorized');
     }
+    // {
+    //     if (!Auth::check()) {
+    //         return redirect()->route('login');
+    //     }
+
+    //     $user = Auth::user();
+
+    //     if ($user->hasRole($role)) {
+    //         return $next($request);
+    //     }
+
+    //     return redirect()->route('unauthorized');
+    // }
 }
--- app/Models/UserProyek.php
+++ app/Models/UserProyek.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Models\Role;
 
 class UserProyek extends Model
 {
@@ -11,5 +12,17 @@ class UserProyek extends Model
     protected $table = "user_proyeks";
     protected $guarded = ["id"];
 
-    
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function proyek()
+    {
+        return $this->belongsTo(Proyek::class, 'proyek_id');
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
